6b789c22c23cea52e6dbc2093198ab747ffb4882
unsigned-decimal <afakir@live.nl>
2020-09-29 14:11:04 +0200

    Initial approach implemented to build a pMC from a POMDP

--- src/main.py
+++ src/main.py
@@ -1,14 +1,29 @@
 from model import Model
+import stormpy.examples
+import stormpy.examples.files
 
 def main():
-    m = Model("../models/maze.prism") 
-    p = "Rmin=?[F s=10 ]"
-    m.parse_properties(p)  
-    m.build_model()
-    m.print_model_type()
-    m.print_transitions() 
-    print("nr_states:", m.model.nr_states)
-    print("nr_observations:", m.model.nr_observations)
+    path1 = "../models/maze.prism"
+    path2 = "../models/maze.drn"
+    #path3 = "../models/die.jani"
+    m = Model(path1) 
+
+    p1 = "P=?[F \"goal\"]"
+    p2 = "Rmin=?[F \"goal\"]"
+    m.parse_properties(p2)
+    m.build_model(properties=True)
+    m.build_pmc()
+    print(m.pmc)
+    result = m.model_checking()
+    print(result)
+    
+
+    
+    #m.print_model_type()
+    #m.print_nr_states()
+    #m.print_actions()
+    #m.print_transitions() 
+    
     
 
 if __name__ == "__main__":
--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 import os
 import stormpy
+import stormpy.pomdp
 
 class Model:
 
@@ -8,32 +9,63 @@ class Model:
         self.properties = None
         self.program = self.load_model(model_file)
         self.model = None
+        self.pmc = None
         
     def load_model(self, model_file):
         self.properties = None
         self.file_type = os.path.splitext(model_file)[1]
         if self.file_type == ".prism":
             return stormpy.parse_prism_program(model_file)
+        elif self.file_type == ".drn":
+            return model_file
+        elif self.file_type == ".jani":
+            jani_program, self.properties = stormpy.parse_jani_model(model_file)
+            return jani_program
         else:
             raise Exception("Model file type not supported") 
 
     def parse_properties(self, properties):
         if self.file_type == ".prism":
-            self.properties = stormpy.parse_properties_for_prism_program(properties, self.program, None)
+            self.properties = stormpy.parse_properties_for_prism_program(properties, self.program)
+        elif self.file_type == ".jani":
+            self.properties = stormpy.parse_properties_for_jani_model(properties, self.program)
+        elif self.file_type == ".drn":
+            self.properties = stormpy.parse_properties_without_context(properties)
         else:
             raise Exception("Model file type not supported")      
 
-    def build_model(self):
-        if self.file_type == ".prism":
-            self.model = stormpy.build_model(self.program, self.properties)
+    def build_model(self, properties=False):
+        if self.file_type == ".prism" or self.file_type == ".jani":
+            self.model = stormpy.build_model(self.program, self.properties if properties else None)
+        elif self.file_type == ".drn":
+            self.model = stormpy.build_model_from_drn(self.program)
         else:
             raise Exception("Model file type not supported") 
-    
+
+    def build_pmc(self):
+        #self.model = stormpy.pomdp.make_canonic(self.model)
+        self.model = stormpy.pomdp.make_simple(self.model)
+        memoryBuilder = stormpy.pomdp.PomdpMemoryBuilder()
+
+        memory = memoryBuilder.build(stormpy.pomdp.PomdpMemoryPattern.selective_counter, 2)
+        self.model = stormpy.pomdp.unfold_memory(self.model, memory)
+        self.pmc = stormpy.pomdp.apply_unknown_fsc \
+            (self.model, stormpy.pomdp.PomdpFscApplicationMode.simple_linear)
+
+    def model_checking(self):
+        return stormpy.model_checking(self.pmc, self.properties[0])
 
     # Print functions
     def print_model_type(self):
-        print(self.model.model_type)
+        print("Model type:",self.model.model_type)
+
+    def print_nr_states(self):
+        print("Number of states:", self.model.nr_states)
     
+    def print_actions(self):
+        for state in self.model.states:
+            print("State", state, " has actions", [str(action) for action in state.actions])
+
     def print_transitions(self):
         for state in self.model.states:
             for action in state.actions:
